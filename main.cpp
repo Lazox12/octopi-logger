241c72a91353eb06911e51fdedfd4a4c94337c52
Lazox12 <tomik.lazar@gmail.com>
2024-12-08 17:42:37 +0100

    fix

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
-#include <algorithm>
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 int main(int argc, char **argv) {
-    std::cout<<argc<<std::endl;
     fstream file(argv[1],ios::app);
     for(int i=2;i<argc;i++) {
         file<<argv[i]<<" ";
